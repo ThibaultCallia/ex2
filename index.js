9467dcfd6e546be9c79ba5e5d1bf65f77d8d5de0
Thibault Calliauw <calliauw.t@gmail.com>
2022-11-15 16:53:52 +0100

    Finish in touches

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ console.log(text1.str);
 console.log("\nleft 3");
 console.log(text1.left(3));
 
-console.log("\nright 3");
-console.log(text1.right(3));
+console.log("\nright 5");
+console.log(text1.right(5));
 
 console.log("\ncharcount");
 console.log(text1.charCount());
@@ -25,5 +25,5 @@ console.log(text1.capitalCount());
 
 console.log("\ntest David prepend and append");
 console.log(
-  new Text("een zin").prepend("Dit is ").append(" in het Nederlands.")
+  new Text("een zin").prepend("Dit is ").append(" in het Nederlands.").str
 );
